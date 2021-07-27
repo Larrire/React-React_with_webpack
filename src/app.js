a0467ffed3ae556f209cba164e72b0e0b49a53e5
Larrire <linekerlarrire@gmail.com>
2021-07-27 09:14:22 -0300

    Aprendendo sobre webpack+react

---
+++ src/app.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import React from 'react';
+
+const App = React.createClass({
+    render: function(){
+        return <div>
+            Aplicação 3
+        </div>
+    }
+});
+
+export default App;
